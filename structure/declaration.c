01befc8d8f7f45ce2349223088c9e573f555fa1d
JonesBeach <47111201+hyperjones@users.noreply.github.com>
2022-09-13 09:51:07 +0800

    Create declaration.c

---
+++ structure/declaration.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define LEN 25
+
+struct
+{
+	int number;
+	char name[LEN + 1];
+	int on_hand;
+}part1 = { 528, "Disk drive", 10 }, 
+ part2 = { 914, "Printer cabinet", 5 };
+
+/*
+   struct part
+   {
+     int number;
+     char name[LEN + 1];
+     int on_hand;
+   }
+*/
+
+
+
+
+
+
+	
+
