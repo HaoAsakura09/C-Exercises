27768619e8f47fa003647ce4ae9eae7ca033e747
JonesBeach <47111201+hyperjones@users.noreply.github.com>
2022-09-13 10:03:18 +0800

    Create designator.c

---
+++ structure/designator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define LEN 25
+
+struct
+{
+	int number;
+	char name[LEN + 1];
+	int on_hand;
+}part1 = { .number = 528, .name = "Disk drive", .on_hand = 10 }, 
+ part2 = { .number = 914, .name = "Printer cabinet", .on_hand = 5 };
+
+int main(void)
+{
+	
+	return 0;
+}
