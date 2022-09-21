5a3bb5d7a20948479d7c95f1b0a6922d34c6445d
JonesBeach <47111201+hyperjones@users.noreply.github.com>
2022-09-21 15:51:30 +0800

    Create matching_braces.c

---
+++ source/matching_braces.c
@@ -0,0 +1,99 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define STACK_SIZE 100
+
+char contents[STACK_SIZE];
+int top = 0;
+
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(char ch);
+int pop(void);
+
+int main(void)
+{
+	bool is_match = true;
+	char ch;
+
+	int pos;
+	printf("Enter parentheses and/or braces: ");
+	while ((ch = getchar()) != '\n')
+	{
+		if (ch == ' ')
+			continue;
+
+		push(ch);
+		switch (ch)
+		{
+		case ')':
+		{
+			pos = pop() - 1;
+			if (contents[pos] == '(' && contents[pos + 1] == ')')
+				is_match = true;
+			else
+				is_match = false;
+		}
+		break;
+		case '}':
+		{
+			pos = pop() - 1;
+			if (contents[pos] == '{' && contents[pos + 1] == '}')
+				is_match = true;
+			else
+				is_match = false;
+		}break;
+		}
+
+		//terminate early if the pair doesn't match
+		if(is_match == false)
+			break;
+	}
+
+	if (is_match)
+		printf("Parenthesis/braces are nested properly.\n");
+	else
+		printf("Parenthesis/braces are not nested properly.\n");
+	return 0;
+}
+
+bool is_empty(void)
+{
+	if (top == 0)
+		return true;
+	return false;
+}
+
+bool is_full(void)
+{
+	if (top == 100)
+		return true;
+	return false;
+}
+
+void push(char ch)
+{
+	if (is_full())
+	{
+		printf("Stack Overflow!\n");
+		return;
+	}
+	else
+	{
+		contents[top++] = ch;
+	}
+}
+
+int pop(void)
+{
+	if (is_empty())
+	{
+		printf("Stack Underflow!\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		return --top;
+	}
+}
