31172a809aabeb91952c4a593e269fdb7b21d22d
JonesBeach <47111201+hyperjones@users.noreply.github.com>
2022-09-28 14:40:09 +0800

    Update dynamic_array.c

--- source/dynamic_array.c
+++ source/dynamic_array.c
@@ -3,20 +3,40 @@
 
 int main(void)
 {
-	int n = 10;
-	int *a = malloc(n * sizeof(n));
+	size_t aSize = 10;
+	int *a = calloc(aSize, sizeof(int));
 	
 	if(a == NULL)
 	{
 		printf("Malloc failed to alocate memory for a.\n");
 		return 0;
 	}
-	printf("Enter %d integers\n", n);
-	for(int i = 0; i < n; i++)
-		scanf("%d", &a[i]);
-		
-	printf("Your entered");
-	for(int i = 0; i < n; i++)
-		printf(" %d", a[i]);
+	
+  int e, i = 0;
+  printf("Enter %lu integers: ", aSize);
+  scanf("%d", &e);
+  while(e != 0)
+  {
+  	if(i < aSize)
+  		a[i] = e;
+  	 else
+  	 {
+  	 	printf("Allocating more space for a.\n");
+  	 	aSize *= 2;
+  	 	a = realloc(a, aSize * sizeof(int));
+  	 	if(a == NULL)
+  	 	{
+  	 		printf("Can't allocate more space.\n");
+  	 		return 0;
+  	 	}
+  	 }
+  	 scanf("%d", &e);
+  	 i++;
+  }
+  
+  printf("You entered:");
+  int z;
+  for(z = 0; z < i; z++)
+  	printf(" %d", a[z]);
 	return 0;
 }
