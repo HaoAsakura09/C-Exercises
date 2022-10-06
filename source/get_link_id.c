5fa6409898f5ee8300dd854137c81357a2664e42
JonesBeach <47111201+hyperjones@users.noreply.github.com>
2022-10-06 18:08:46 +0800

    Create get_link_id.c

---
+++ source/get_link_id.c
@@ -0,0 +1,57 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define LINK_LEN 50
+char *get_id(char *link);
+
+int read_line(char link[], int len);
+
+int main(void)
+{
+	char yt_link[LINK_LEN], *p;
+	
+	read_line(yt_link, LINK_LEN);
+	
+	p = get_id(yt_link);
+	
+//	printf("%s", p);
+	return 0;
+}
+
+int read_line(char link[], int len)
+{
+	int i = 0, ch;
+	while(isspace(ch = getchar()))
+	 	;
+	 while(ch != '\n' && ch != EOF)
+	 {
+	 	if(i < len)
+	 		link[i++] = ch;
+	 	ch = getchar();
+	 }
+	 link[i] = '\0';
+	 return i;
+}
+
+char *get_id(char *link)
+{
+	
+	char *p = link, *result;
+	result = malloc(strlen(link) + 1);
+	if(result == NULL)
+	{
+		printf("Memory allocation failed.\n");
+		exit(EXIT_FAILURE);
+	}
+	 
+	 while(*p != '\0')
+	 	p++;
+   
+   while(*p != '=')
+   	p--;
+	strcpy(result, p);
+	
+	return result;
+}
